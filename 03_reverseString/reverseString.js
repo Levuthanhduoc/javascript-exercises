10f2b2112c5b5b8f1fefedcd0e99c410ab73ed9c
levuthanhduoc <levuthanhduoc@gmail.com>
2023-07-26 16:52:47 +0700

    Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
-};
+const reverseString = function(texts){
+    let revertext='';
+    let textLeght = 0;
+    for(let text of texts){
+        textLeght++;
+    }
+    for(let i = 1;i <= textLeght;i++)
+    {
+        revertext += texts[textLeght-i];
+    }
+    return revertext;
+} ;
 
 // Do not edit below this line
 module.exports = reverseString;
