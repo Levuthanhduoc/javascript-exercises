9f302d90e330cc90a3d5f240acf2637bbd6c913c
levuthanhduoc <levuthanhduoc@gmail.com>
2023-07-26 16:21:14 +0700

    DONE

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(RepeatText,RepeatTime) {
+    let resultText ='';
+    if(RepeatTime <0){
+        return "ERROR";
+    }
+    else if(RepeatTime == 0){
+        return "";
+    }
+    for(let i = 0; i < Math.abs(RepeatTime);i++)
+    {
+        resultText = resultText + RepeatText;
+    }
+    return resultText;
 };
 
 // Do not edit below this line
