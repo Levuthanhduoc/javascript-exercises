0513921f5a4d8aeb1562708e787a11e0f0fb9a26
levuthanhduoc <levuthanhduoc@gmail.com>
2023-08-05 19:26:42 +0700

    DONE

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
 const add = function() {
-	
+  a = Number(arguments[0]);
+  b = Number(arguments[1]);
+	return a + b;
 };
 
 const subtract = function() {
-	
+	a = Number(arguments[0]);
+  b = Number(arguments[1]);
+	return a - b;
 };
 
 const sum = function() {
-	
+	arr = arguments[0];
+	return arr.reduce((total,item)=> total + item,0);
 };
 
 const multiply = function() {
-
+  arr = arguments[0];
+	return arr.reduce((total,item)=> total * item);
 };
 
 const power = function() {
-	
+	a = Number(arguments[0]);
+  b = Number(arguments[1]);
+	return a ** b;
 };
 
 const factorial = function() {
-	
+  let factor = 1;
+	for(let i = arguments[0]; i > 0; i--){
+    factor *= i;
+  }
+  return factor;
 };
 
 // Do not edit below this line
